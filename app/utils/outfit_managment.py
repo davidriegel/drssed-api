49a3ee6d427b45c4ff61440a2630046716230b2b
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-04-28 19:16:25 +0200

    fix: remove extra placeholder in outfit creation SQL query

--- app/utils/outfit_managment.py
+++ app/utils/outfit_managment.py
@@ -204,9 +204,9 @@ class OutfitManager:
                 cursor = conn.cursor()
                 cursor.execute("""
                     INSERT INTO outfits(outfit_id, is_public, is_favorite, name, user_id, description)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s);
+                    VALUES (%s, %s, %s, %s, %s, %s);
                 """, (
-                    outfit_id, is_public, is_favorite, name, user_id, description, 
+                    outfit_id, is_public, is_favorite, name, user_id, description,
                 ))
 
                 if outfit.seasons:
