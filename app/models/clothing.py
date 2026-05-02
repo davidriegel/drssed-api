75213392b3daa6fcdc93167a187eeeb3e556743c
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-05-02 21:47:23 +0200

    refactor: standardize clothing sub-category string values to uppercase

--- app/models/clothing.py
+++ app/models/clothing.py
@@ -23,24 +23,24 @@ class ClothingCategory(str, Enum):
     
 class ClothingSubCategory(str, Enum):
     # TOP
-    T_SHIRT = "t-shirt"
-    SHIRT = "shirt"
-    POLO_SHIRT = "polo shirt"
-    SWEATER = "sweater"
-    HOODIE = "hoodie"
+    T_SHIRT = "T-SHIRT"
+    SHIRT = "SHIRT"
+    POLO_SHIRT = "POLO_SHIRT"
+    SWEATER = "SWEATER"
+    HOODIE = "HOODIE"
     # BOTTOM
-    JEANS = "jeans"
-    TROUSERS = "trousers"
-    SHORTS = "shorts"
-    SKIRT = "skirt"
+    JEANS = "JEANS"
+    TROUSERS = "TROUSERS"
+    SHORTS = "SHORTS"
+    SKIRT = "SKIRT"
     # JACKET
-    JACKET = "jacket"
-    DENIM_JACKET = "denim jacket"
-    SPORTS_JACKET = "sports jacket"
-    COAT = "coat"
-    BLAZER = "blazer"
+    JACKET = "JACKET"
+    DENIM_JACKET = "DENIM_JACKET"
+    SPORTS_JACKET = "SPORTS_JACKET"
+    COAT = "COAT"
+    BLAZER = "BLAZER"
     # ONE_PIECE
-    DRESS = "dress"
+    DRESS = "DRESS"
 
 @dataclass
 class Clothing:
