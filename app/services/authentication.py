6b493607fa7079030bf88eaefd6af323081ef59f
David <40246197+davidriegel@users.noreply.github.com>
2026-08-06 12:04:57 +0200

    fix: allow null email and password hash in user schemas so guests do not crash (#34)

--- app/services/authentication.py
+++ app/services/authentication.py
@@ -177,7 +177,7 @@ class AuthenticationManager:
         else:
             user_sign_in = None
 
-        if not user_sign_in:
+        if not user_sign_in or not user_sign_in.password_hash:
             raise UnauthorizedError
 
         try:
