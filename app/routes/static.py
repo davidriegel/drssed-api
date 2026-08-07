27182b1f9343c40832e3c870b47760fbdbd38f3a
David Riegel <40246197+davidriegel@users.noreply.github.com>
2026-08-07 14:18:28 +0200

    feat: require authentication for every image request

--- app/routes/static.py
+++ app/routes/static.py
@@ -1,25 +1,26 @@
 from flask import Blueprint, jsonify, send_from_directory
+from werkzeug.exceptions import NotFound
 
 from app.core.limiter import limiter
-from app.core.logging import get_logger
+from app.utils.middleware.authentication import authorize_request
 
 static = Blueprint("static", __name__)
-logger = get_logger()
 
 
 @static.route("/clothing_images/<clothing_id>.webp", methods=["GET"])
 @static.route("/clothing_images/<clothing_id>", methods=["GET"])
 @limiter.limit("10 per minute")
+@authorize_request
 def getClothingImage(clothing_id):
     try:
         return send_from_directory("app/static/clothing_images", f"{clothing_id}.webp")
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
-        return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
+    except NotFound:
+        return jsonify({"error": "File not found."}), 404
 
 
 @static.route("/temp/<filename>", methods=["GET"])
 @limiter.limit("2 per minute")
+@authorize_request
 def getTempImage(filename):
     if not filename:
         return jsonify({"error": "Filename is required"}), 400
@@ -30,13 +31,13 @@ def getTempImage(filename):
 
     try:
         return send_from_directory("app/static/temp", f"{filename}")
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
-        return jsonify({"error": "File not found."}), 500
+    except NotFound:
+        return jsonify({"error": "File not found."}), 404
 
 
 @static.route("/outfit_images/<filename>", methods=["GET"])
 @limiter.limit("10 per minute")
+@authorize_request
 def get_outfit_image(filename):
     filename = (
         filename.strip() + ".webp" if not filename.endswith(".webp") else filename
@@ -44,6 +45,19 @@ def get_outfit_image(filename):
 
     try:
         return send_from_directory("app/static/outfit_collages", f"{filename}")
-    except Exception as e:
-        logger.error(f"An unexpected error occurred: {e}")
-        return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
+    except NotFound:
+        return jsonify({"error": "File not found."}), 404
+
+
+@static.route("/profile_pictures/<filename>", methods=["GET"])
+@limiter.limit("60 per minute")
+@authorize_request
+def get_profile_picture(filename):
+    filename = (
+        filename.strip() + ".webp" if not filename.endswith(".webp") else filename
+    )
+
+    try:
+        return send_from_directory("app/static/profile_pictures", f"{filename}")
+    except NotFound:
+        return jsonify({"error": "File not found."}), 404
